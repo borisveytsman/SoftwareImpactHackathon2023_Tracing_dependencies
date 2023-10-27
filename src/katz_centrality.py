6b14f7f7bcb3f1291bbda89f12ef62e6b3d07375
LaurentHebert <laurent.hebert.dufresne@gmail.com>
2023-10-27 08:20:19 -0700

    Added Katz centrality

---
+++ src/katz_centrality.py
@@ -0,0 +1,11 @@
+import networkx as nx
+
+def get_katz_centrality(graph):
+    
+    # Calculate max eigenvalue of adjacency matrix for convergence
+    phi = max(nx.adjacency_spectrum(graph, weight= 'weight'))
+    # Iterative calculation of Katz centrality (beta is the importance of papers)
+    centrality = nx.katz_centrality(G, alpha = 1 / phi - 0.01, beta=1.0,  weight= 'weight',  max_iter = 100000)
+
+    # Returns the dictionary containing the centrality of each node
+    return centrality
